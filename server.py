bf4c92b86dd367087de2009ed3fe8366bfda79e4
abdelrahmanyasser10 <abdelrahmanyasser101112@gmail.com>
2023-08-26 22:32:06 +0300

    added the last_packet and the next_packet

--- server.py
+++ server.py
@@ -6,12 +6,18 @@ print("the server is available")
 server_socket= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((Host, Port))
 
+last_packet = 0
+
 while True:
     data, client_address = server_socket.recvfrom(1024)
-    if data == "exit":
+    if not data:
             break
     else:
         print(f"{data.decode()} is received")
+        next_packet = int(data.decode())
         server_socket.sendto(data, client_address)
+    if next_packet == last_packet + 1:
+        last_packet = next_packet
+
 server_socket.close()
 
