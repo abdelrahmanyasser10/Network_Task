41b046c8c07f24aef8e7947fa028925f0aeab2a0
Asmaa <es-asmaa.ghanem2026@alexu.edu.eg>
2023-08-26 22:44:34 +0300

    code for client sending number to server

---
+++ UDPclient.py
@@ -0,0 +1,11 @@
+import socket as sk
+
+socket_c=sk.socket(sk.AF_INET,sk.SOCK_DGRAM)
+socket_c.connect((' 127.0.8.1'), 9999 ) #address and port id of server
+
+starting_number =0
+while 1:
+    starting_number+=1
+    socket_c.sendto(starting_number.encode('utf-8'),9999)
+    server_msg=socket_c.recvfrom(1024)
+    print(server_msg[0])
