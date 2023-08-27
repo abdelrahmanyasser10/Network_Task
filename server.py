02c4e249fdc51908d661150861377566e872ff65
Asmaa <es-asmaa.ghanem2026@alexu.edu.eg>
2023-08-27 14:46:27 +0300

    Modified receiver file, ready to test

--- server.py
+++ server.py
@@ -9,31 +9,37 @@ print("the server is available")
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((Host, Port))
 
-with open("server.txt", "a") as f:
+file_name ='server.txt'
+with open(file_name, 'a') as f:
     for _ in range(0, 100):
         f.write('\n')
+#created 100 new lines in file
 
-with open('server.txt', 'r') as f:
+with open(file_name, 'r') as f:
     lines = f.readlines()
+#read lines and return them as strings in list
 
-with open('server.txt', 'w') as f:
-    f.write(lines[0])
+with open(file_name, 'w') as f:
+    f.write(lines[0]) #new line 
     for line in lines[1:]:
         random_number = random.randrange(0,100)
         f.write(str(random_number) + line)
+#filling 99 lines with 99 random numbers in new lines
 
-with open('server.txt', 'r') as f:
+with open(file_name, 'r') as f:
     lines = f.readlines()
-with open('server.txt', 'w') as f:
+#read lines after filling and store in lines list 
+
+with open(file_name, 'w') as f:
     f.write(lines[0])
     for line in lines[1:]:
         random_char = random.choice(string.ascii_letters)
         f.write(random_char + line)
-
+#adding random ascii characters to each line 
 
 global file_size
 try:
-    file_size = os.path.getsize("server.txt")
+    file_size = os.path.getsize(file_name)
     print(f"File Size in Bytes is {file_size}")
 except FileNotFoundError:
     print("File not found.")
@@ -48,7 +54,7 @@ while True:
         server_socket.sendto(str(file_size).encode('utf-8'), receiver_address)
 
     else:
-        with open("server.txt", 'rb') as f:
+        with open(file_name, 'rb') as f:
             byte = f.read(1)
             while byte != b"":
                 print(byte + b' is sent')
